032124c48e4d79c746dbbf9f77120258e9bcb630
Shaqeeb Akhtar <shaqeebakhtar01@gmail.com>
2024-01-26 21:40:27 +0530

    spawn enemies

--- script.js
+++ script.js
@@ -73,6 +73,33 @@ const y = canvas.height / 2;
 const player = new Player(x, y, 30, 'blue');
 
 const projectiles = [];
+const enemies = [];
+
+const spawnEnemies = () => {
+  setInterval(() => {
+    const radius = Math.random() * (30 - 4) + 4;
+
+    let x, y;
+
+    if (Math.random() < 0.5) {
+      x = Math.random() < 0.5 ? 0 - radius : canvas.width + radius;
+      y = Math.random() * canvas.height;
+    } else {
+      x = Math.random() * canvas.width;
+      y = Math.random() < 0.5 ? 0 - radius : canvas.height + radius;
+    }
+
+    const color = 'green';
+
+    const angle = Math.atan2(canvas.height / 2 - y, canvas.width / 2 - x);
+    const velocity = {
+      x: Math.cos(angle),
+      y: Math.sin(angle),
+    };
+
+    enemies.push(new Enemy(x, y, radius, color, velocity));
+  }, 1000);
+};
 
 const animate = () => {
   requestAnimationFrame(animate);
@@ -80,6 +107,9 @@ const animate = () => {
   player.draw();
   projectiles.forEach((projectile) => {
     projectile.update();
+  });
+  enemies.forEach((enemy) => {
+    enemy.update();
   });
 };
 
@@ -100,3 +130,4 @@ addEventListener('click', (event) => {
 });
 
 animate();
+spawnEnemies();
